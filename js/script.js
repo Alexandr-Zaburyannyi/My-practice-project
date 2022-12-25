3d4645c32807c117a5f5b0797c9b173ee7aec32c
Alexandr-Zaburiannyi <ozaburiannyi@gmail.com>
2022-12-25 20:17:53 +0200

    new functionality was added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-"use strict";
+'use strict';
+
+(function () {
+  const colorRadioBtns = document.querySelectorAll('.color-radio-btn');
+
+  //functions
+  colorRadioBtns.forEach(btn => {
+    btn.classList.add('radio-btn-color-border');
+    btn.classList.remove('border-dark');
+    btn.addEventListener('click', e => {
+      if (e.currentTarget.classList.contains('radio-btn-color-border')) {
+        colorRadioBtns.forEach(butn => butn.classList.remove('border-dark'));
+        btn.classList.add('border-dark');
+      } else {
+        return;
+      }
+    });
+  });
+})();
